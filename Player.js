668ac4fdb82e6f4255c3e0f655496f8f3ef1ffd2
Deepak <deepakmishra88@yahoo.com>
2021-02-10 15:27:28 +0100

    increase re raise amount

--- Player.js
+++ Player.js
@@ -4,7 +4,7 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    const toBet = Math.floor(Math.random() * 20) + 1;
+    const toBet = Math.floor(Math.random() * 50) + 1;
     const toRaise = gameState.minimum_raise;
 
     if (toRaise <= 500) {
