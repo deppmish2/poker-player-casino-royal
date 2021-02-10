9b2845b82c5cae3bfb9ed789644c8136a3e8949a
Dyl <dyl@touche.local>
2021-02-10 11:40:37 +0100

    first push ftw

--- Player.js
+++ Player.js
@@ -1,14 +1,15 @@
+const GameState = require("./Game");
+
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    bet(100);
   }
 
-  static showdown(gameState) {
-  }
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
