600d87e95010cac81352216f86061f3ec2657416
Dyl <dyl@touche.local>
2021-02-10 14:08:04 +0100

    smort

--- Player.js
+++ Player.js
@@ -5,8 +5,12 @@ class Player {
 
   static betRequest(gameState, bet) {
     const toBet = Math.floor(Math.random() * 250) + 1;
+    const toRaise =
+      gameState.current_buy_in -
+      gameState.players[in_action][bet] +
+      gameState.minimum_raise;
 
-    bet(toBet);
+    bet(toBet + toRaise);
   }
 
   static showdown(gameState) {}
